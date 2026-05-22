5e16d3e8ef560d7e74e66e9bcdfda0e342f69432
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2026-05-22 11:04:16 +0530

    lenis added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import Lenis from "lenis";
+
+// Lenis Setup
+const lenis = new Lenis({
+  duration: 1,
+  // lerp: 0.08,
+  wheelMultiplier: 0.9,
+  smoothWheel: true,
+  smoothTouch: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
